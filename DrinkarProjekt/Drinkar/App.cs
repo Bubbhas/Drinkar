7f6ef5c328e1a17ce7545af70d8fb65a6972f835
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-05 11:38:09 +0200

    Run App osv

---
+++ DrinkarProjekt/Drinkar/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drinkar
+{
+   public  class App
+    {
+        public void Run()
+        {
+            List<string> usersIngredients = new List<string>();
+            Console.WriteLine("Welcome!\nAre you thirsty?    Of course you are!\nLet us recommend you a cocktail depending on what you have!");
+            Console.Write("Enter ingredients separated with a comma(,): ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello from Anastasia");
+        }
+        
+    }
+}
