356c9cb7c531e7a13f6f44a0b883e89cf85185af
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-09-05 11:40:43 +0200

    nytt program och run

--- DrinkarProjekt/Drinkar/Program.cs
+++ DrinkarProjekt/Drinkar/Program.cs
@@ -7,12 +7,8 @@ namespace Drinkar
     {
         static void Main(string[] args)
         {
-            List<string> usersIngredients = new List<string>();
-            Console.WriteLine("Welcome!\nAre you thirsty?    Of course you are!\nLet us recommend you a cocktail depending on what you have!");
-            Console.Write("Enter ingredients separated with a comma(,): ");
-            string input = Console.ReadLine();
-
-            Console.WriteLine("Hello from Anastasia");
+            new App().Run();
+            
 
         }
     }
