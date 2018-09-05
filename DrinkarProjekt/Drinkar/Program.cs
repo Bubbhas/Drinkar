57d42d806a14d8776b20f4ae83c527e5c4d6c2ba
Christoffer J. Öhrström <christoffer.j.ohrstrom@gmail.com>
2018-09-05 10:05:41 +0200

    Create project

---
+++ DrinkarProjekt/Drinkar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Drinkar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
