5c8be1b8c7176bdb99c377e7d85c94afe1edd3d5
AWA5CG8126DJQ\Administrator <anastasia.alekseeva@gmail.com>
2018-09-07 13:46:33 +0200

    ..

--- DrinkarProjekt/Drinkar/App.cs
+++ DrinkarProjekt/Drinkar/App.cs
@@ -260,41 +260,7 @@ namespace Drinkar
             Console.Write("{0," + ((Console.WindowWidth / 2) + s.Length / 2) + "}", s);
         }
 
-        private void ChooseDrink()
-        {
-            Console.WriteLine("Can you decide now which cocktail do you want to make, if so press (a). Overwise, press (b) for getting some advice.");
-            string response = Console.ReadLine();
-            if (response == "a")
-            {
-                Console.WriteLine("Choose the number of a cocktail you want to make:");
-                int answer = int.Parse(Console.ReadLine());
-
-                //var dataAccess = new DataAccess();
-
-                //Drink matchedDrinks = dataAccess.GetCocktailNameById(answer);
-
-                //Console.WriteLine(dataAccess.GetCocktailNameById(description));
-            }
-            else if (response == "b")
-            {
-                Console.WriteLine("What kind of drink do you want? /n(a) Sweet /n(b) Sour /n(c) Extra alcohol ");
-                string cocktailKind = Console.ReadLine();
-                //if (cocktailKind == "a")
-                //    Console.WriteLine(DataAccess.GetCocktailByKind(sweet));
-                //else if (cocktailKind == "b")
-                //    Console.WriteLine(DataAccess.GetCocktailByKind(sour));
-                //else if (cocktailKind == "c")
-                //    Console.WriteLine(DataAccess.GetCocktailByKind());extraAlcohol
-                //else
-                //{
-                //    Console.Clear();
-                //    ChooseDrink();
-                //}
-            }
-            else
-                Console.Clear();
-            PageMainMenu();
-        }
+        
         private void ShowDrinkInstructions(int input)
         {
             Console.WriteLine("Vill du sätta igång med drinkgörandet? (J)a eller (N)ej?");
@@ -356,19 +322,7 @@ namespace Drinkar
             }
         }
 
-        //private List<Drink> ShowAllMatchedDrinks(string[] ingrediences)
-        //{
-        //    Console.Clear();
-
-        //    var dataAccess = new DataAccess();
-        //    List<Drink> matched = dataAccess.GetAllDrinksWithIngredient(ingrediences, ingrediences.Length);
-
-        //    foreach (var drink in matched)
-        //    {
-        //        Console.WriteLine(drink.Id + " " + drink.Name + " " + drink.Description);
-        //    }
-        //    return matched;
-        //}
+        
 
     }
 }
